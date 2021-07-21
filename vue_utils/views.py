c7ce0650dc09c3afc6f3ea754e70c867f7294772
FairWind <sergey.manenok@gmail.com>
2021-07-21 11:45:58 +0300

    Add CrudView

--- vue_utils/views.py
+++ vue_utils/views.py
@@ -1,12 +1,17 @@
-from django.http import Http404, JsonResponse
+import django
+from django.forms import CharField, HiddenInput, Field
+from django.http import Http404, JsonResponse, HttpResponseNotAllowed, HttpResponseBadRequest
 from django.shortcuts import render
 from django.utils.translation import gettext as lang
 # Create your views here.
-from django.views.generic import ListView
-
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic.edit import FormMixin, DeletionMixin, ProcessFormView
+from django.views.generic.detail import SingleObjectTemplateResponseMixin, DetailView, BaseDetailView
+from django.views.generic.base import ContextMixin
 from vue_utils.request_processsor.filter_creators import compute_filters
 from vue_utils.request_processsor.paging import paging_queryset
 from vue_utils.request_processsor.serialize import my_serializer, serialize_queryset
+from vue_utils.request_processsor.utility import get_from_request
 from vue_utils.request_processsor.view import exec_filter_sort_view_queryset
 
 
@@ -73,7 +78,7 @@ class FilterListView(ListView):
         return {}
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(object_list=object_list, **kwargs)
+        context = super(FilterListView, self).get_context_data(object_list=object_list, **kwargs)
         add_context = self.get_additional_context_attribute()
         if add_context:
             context.update(**add_context)
@@ -83,6 +88,8 @@ class FilterListView(ListView):
         if self.filter_form_values:
             context.update(**{'filter_form_values': self.filter_form_values})
         return context
+
+       
 
     def get_queryset(self):
         list_objects = super().get_queryset()
@@ -112,13 +119,176 @@ class FilterListView(ListView):
 
     def get(self, request, *args, **kwargs):
         self.last_request = request
-        return super(FilterListView, self).get(request, *args, *kwargs)
+        return ListView.get(self, request, *args, *kwargs)
+        # return super(FilterListView, self).get(request, *args, *kwargs)
 
     def post(self, request, *args, **kwargs):
         # self.last_request = request
-        # return super(FilterListView, self).get(request, *args, *kwargs)
         return self.get(request, *args, **kwargs)
 
+
+class CrudView(CreateView, UpdateView, DeleteView, DetailView):
+    operation_mark_field = 'operation'
+    form_template_name_suffix = '_form'
+    delete_template_name_suffix = '_form'
+    detail_template_name_suffix = '_detail'
+    use_self_success_url = True
+
+    def __init__(self, *args, **kwargs):
+        self.operation = None
+        self.object = None
+        self.csrf_token_for_form = None
+        self.request_path = None
+        super().__init__(*args, **kwargs)
+
+    def view_object(self, *args, **kwargs):
+        context = self.get_context_data()
+        print(context)
+        return self.render_to_response(context)
+
+    def create_operation(self, request, *arg, **kwarg):
+        return ProcessFormView.post(self, request, *arg, **kwarg)
+
+    def update_operation(self, request, *arg, **kwarg):
+        self.object = self.get_object()
+        return ProcessFormView.post(self, request, *arg, **kwarg)
+
+    def delete_operation(self, request, *arg, **kwarg):
+        return DeletionMixin.delete(self, request, *arg, **kwarg)
+
+    def view_for_create(self, request, *arg, **kwarg):
+        return self.view_object()
+
+    def view_for_update(self, request, *arg, **kwarg):
+        return self.view_object()
+        # return BaseDetailView.get(self, request, *arg, **kwarg)
+
+    def view_for_delete(self, request, *arg, **kwarg):
+        return self.view_object()
+
+    def view_for_detail(self, request, *arg, **kwarg):
+        return self.view_object()
+
+    def get_success_url(self):
+        if self.use_self_success_url and self.request_path is not None:
+            return self.request_path
+        else:
+            return super().success_url()
+
+
+    def get_object(self, queryset=None):
+        obj = None
+        if self.operation is not None and self.operation != 'create':
+            obj = super().get_object(queryset)
+            self.object = obj
+        return obj
+
+    def get_context_data(self, **kwargs):
+        if 'object' not in kwargs:
+            obj = self.get_object()
+            if obj:
+                context_object_name = self.get_context_object_name(obj)
+                if context_object_name:
+                    kwargs[context_object_name] = obj
+                else:
+                    kwargs['object'] = obj
+        if 'form' not in kwargs:
+            kwargs['form'] = self.get_form()
+        return ContextMixin.get_context_data(self, **kwargs)
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields[self.operation_mark_field] = Field(initial=self.operation, widget=HiddenInput)
+        if self.csrf_token_for_form is not None:
+            form.fields['csrfmiddlewaretoken'] = Field(initial=self.csrf_token_for_form, widget=HiddenInput)
+        return form
+
+    def check_operation(self, request, default_template_suffics=''):
+        operation, in_request = get_from_request(request, self.operation_mark_field, None)
+        self.operation = operation
+        self.request_path = request.path
+        if in_request:
+            if hasattr(django, 'middleware') and hasattr(django.middleware, 'csrf'):
+                self.csrf_token_for_form = django.middleware.csrf.get_token(request)
+            pk, pk_in_request = get_from_request(request, self.pk_url_kwarg, None)
+            slug, slug_in_request = get_from_request(request, self.slug_url_kwarg, None)
+            if pk_in_request:
+                self.kwargs[self.pk_url_kwarg]=pk
+            if slug_in_request:
+                self.kwargs[self.slug_url_kwarg]=slug
+            if operation == 'create':
+                self.template_name_suffix = self.form_template_name_suffix
+            elif operation == 'update':
+                self.template_name_suffix = self.form_template_name_suffix
+            elif operation == 'delete':
+                self.template_name_suffix = self.delete_template_name_suffix
+            else:
+                self.template_name_suffix = self.detail_template_name_suffix
+        else:
+            self.template_name_suffix = default_template_suffics
+        return operation
+
+    def process_get_request(self, request, *args, **kwargs):
+        if self.operation == 'create':
+            return self.view_for_create(request, *args, **kwargs)
+        elif self.operation == 'update':
+            return self.view_for_update(request, *args, **kwargs)
+        elif self.operation == 'delete':
+            return self.view_for_delete(request, *args, **kwargs)
+        elif self.operation == 'detail':
+            return self.view_for_detail(request, *args, **kwargs)
+        else:
+            return HttpResponseBadRequest()
+
+    def process_post_request(self, request, *args, **kwargs):
+        if self.operation == 'create':
+            return self.create_operation(request, *args, **kwargs)
+        elif self.operation == 'update':
+            return self.update_operation(request, *args, **kwargs)
+        elif self.operation == 'delete':
+            return self.delete_operation(request, *args, **kwargs)
+        else:
+            return HttpResponseBadRequest()
+
+    def get(self, request, *args, **kwargs):
+        self.check_operation(request)
+        return self.process_get_request(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.check_operation(request)
+        return self.process_post_request(request, *args, **kwargs)
+
+
+class CrudListView(FilterListView, CrudView):
+    operation_mark_field = 'operation'
+    list_template_name_suffix = '_list'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        if self.operation is not None:
+            return CrudView.get_context_data(self, **kwargs)
+        else:
+            self.template_name_suffix = self.list_template_name_suffix
+            return FilterListView.get_context_data(self, object_list=object_list, **kwargs)
+        
+    def get_template_names(self):
+        if self.operation is not None:
+            return SingleObjectTemplateResponseMixin.get_template_names(self)
+        else:
+            return FilterListView.get_template_names(self)
+
+    def get(self, request, *args, **kwargs):
+        self.check_operation(request)
+        if self.operation is None:
+            return FilterListView.get(self, request, *args, **kwargs)
+        else:
+            return CrudView.process_get_request(self, request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.check_operation(request)
+        if self.operation is None:
+            return FilterListView.post(self, request, *args, **kwargs)
+        else:
+            return CrudView.process_post_request(self,request, *args, **kwargs)
 
 class FilterAjaxListView(FilterListView):
     # viewed_fields - Поля для по для отображения:
